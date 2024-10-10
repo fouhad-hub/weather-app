691a26909b965a780640cee36d9a91228b90fa78
fouhad-hub <adegbitefouhad@gmail.com>
2024-10-10 11:24:44 +0100

    first javascript implementation

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const apiKey = "53ebeb56216755f71dca8f67d1ec6cf0";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&&units=metric";
 
-async;
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  var data = await response.json();
+  console.log(data);
+}
+checkWeather();
 // fetchData();
 
 // async function fetchData() {
